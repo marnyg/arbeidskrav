88bd4d9e6d129a734231b444ea52b418968d3b8d
Marius Nygard <marnyg31.10@gmail.com>
2018-01-23 19:09:50 +0100

    added select sentences

---
+++ src/Oving7/select setninger.sql
@@ -0,0 +1,14 @@
+#1
+SELECT navn,tlf,epost FROM Bedrift;
+
+#2
+SELECT oppdragsNR,navn,tlf FROM oppdrag,Bedrift where organisasjon=Bedrift.organisasjonNr;
+
+#3
+SELECT Kandidat.kandidatID,Fornavn,Etternavn, kvalifikasjon FROM Kandidat,kvalifikasjon where Kandidat.kandidatID=kvalifikasjon.kandidatID;
+
+#4
+SELECT Kandidat.kandidatID,Fornavn,Etternavn, kvalifikasjon from Kandidat LEFT JOIN kvalifikasjon ON Kandidat.kandidatID = kvalifikasjon.kandidatID
+
+#5
+SELECT Fornavn,Etternavn,sluttdato,oppdrag,navn FROM historik,Kandidat,Bedrift WHERE KandidatID=3 and historik.kandidat=Kandidat.kandidatID and Bedrift.organisasjonNr=historik.oppdrag;
